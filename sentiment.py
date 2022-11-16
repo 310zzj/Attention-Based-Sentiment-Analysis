9ec57993575422582bb2b108685f81dc6c38b7cb
310zzj <310zzj@proton.me>
2022-11-16 08:28:09 +0000

    A fix

--- sentiment.py
+++ sentiment.py
@@ -24,4 +24,11 @@ from keras.layers import Dense, Input, LSTM, Embedding, Dropout, Activation
 from keras.layers.merge import concatenate
 from keras.models import Model
 from keras.layers.normalization import BatchNormalization
-from keras.callbacks import EarlyStopping, ModelCheckp
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+
+
+
+
+import sys
+
+from ke
