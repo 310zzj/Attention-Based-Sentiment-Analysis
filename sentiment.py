c60064aba623726cec13d4f006af5119603f7689
310zzj <310zzj@proton.me>
2022-12-07 08:07:29 +0000

    selinux: handle TCP SYN-ACK packets correctly in selinux_ip_postroute()

--- sentiment.py
+++ sentiment.py
@@ -63,4 +63,5 @@ class Attention(Layer):
         self.W = self.add_weight(name='kernel', 
                                       shape=(input_shape[-1],),
                                       initializer= self.init,
-                                      constraint = self.W_c
+                                      constraint = self.W_constraint,
+                             
