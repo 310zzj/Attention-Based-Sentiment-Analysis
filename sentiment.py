086ead8bbf46de0f310870ad27fec86b00fa8213
310zzj <310zzj@proton.me>
2022-12-22 12:13:01 +0000

    zram: avoid kunmap_atomic() of a NULL pointer

--- sentiment.py
+++ sentiment.py
@@ -87,4 +87,5 @@ class Attention(Layer):
 
         eij = K.reshape(K.dot(K.reshape(x, (-1, features_dim)), K.reshape(self.W, (features_dim, 1))), (-1, step_dim))
 
-        if self.
+        if self.bias:
+            eij += self
