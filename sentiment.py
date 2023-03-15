b9c9a555241e11b25e0f9e163f17575223e580b7
310zzj <310zzj@proton.me>
2023-03-15 16:43:58 +0000

    Removed README.md

--- sentiment.py
+++ sentiment.py
@@ -194,4 +194,4 @@ def preprocess_text(text, remove_stopwords = True, perform_stemming = True):
 ##################################################    
 raw_train_comments = train_df['comments'].fillna('NA').values
 raw_test_comments = test_df['comments'].fillna('NA').values
-cla
+classes_to_predict = ["toxic", "sev
