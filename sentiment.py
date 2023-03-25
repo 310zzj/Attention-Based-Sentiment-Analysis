8f23d54c1d45d8a9c53d39fdcf7118f5da62e5a1
310zzj <310zzj@proton.me>
2023-03-25 20:37:18 +0000

    gradle

--- sentiment.py
+++ sentiment.py
@@ -210,4 +210,5 @@ for comment in raw_test_comments:
 tokenizer = Tokenizer(num_words = MAX_NB_WORDS)
 tokenizer.fit_on_texts(processed_train_comments + processed_test_comments)
 
-train_sequences = tokenizer.text_to_sequences
+train_sequences = tokenizer.text_to_sequences(processed_train_comments)
+test
