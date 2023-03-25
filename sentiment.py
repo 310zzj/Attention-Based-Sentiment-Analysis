c87c5cf4dd42dc406285fd541d3fc3956eb7d323
310zzj <310zzj@proton.me>
2023-03-25 21:12:00 +0000

    python update 2

--- sentiment.py
+++ sentiment.py
@@ -211,4 +211,4 @@ tokenizer = Tokenizer(num_words = MAX_NB_WORDS)
 tokenizer.fit_on_texts(processed_train_comments + processed_test_comments)
 
 train_sequences = tokenizer.text_to_sequences(processed_train_comments)
-test
+test_sequences = tokenizer.text_to_seque
