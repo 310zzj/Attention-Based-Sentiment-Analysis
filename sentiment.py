7a1af172bbb332dedaa3d37043b8934860a13b33
310zzj <310zzj@proton.me>
2023-04-12 14:31:08 +0000

    Version Packages

--- sentiment.py
+++ sentiment.py
@@ -230,4 +230,5 @@ print('shape of label(y) is %s' %(y.shape))
 print('preparing embedding matrix')
 word_index = tokenizer.word_index
 nb_words  = min(MAX_NB_WORDS, len(word_index))
-embedding_matrix = np.zeros((nb_words, EMBEDDI
+embedding_matrix = np.zeros((nb_words, EMBEDDING_DIM))
+for word, i in wor
