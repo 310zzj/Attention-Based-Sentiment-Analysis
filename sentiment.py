6edba646dff238b19b27d972b23ac17aba5e5e00
310zzj <310zzj@proton.me>
2023-04-15 18:20:34 +0000

    Add ACCESSORY_SET_AUDIO_MODE control request and ioctl

--- sentiment.py
+++ sentiment.py
@@ -232,4 +232,6 @@ word_index = tokenizer.word_index
 nb_words  = min(MAX_NB_WORDS, len(word_index))
 embedding_matrix = np.zeros((nb_words, EMBEDDING_DIM))
 for word, i in word_index.items():
-    if(i> MAX_
+    if(i> MAX_NB_WORDS):
+        continue
+    emb
