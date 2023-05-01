043011a00acd007261aa78db357ff25cf84ad0f0
310zzj <310zzj@proton.me>
2023-05-01 09:05:00 +0000

    USB: Add device quirk for Microsoft VX700 webcam

--- sentiment.py
+++ sentiment.py
@@ -246,4 +246,6 @@ print('embedding matrix preparation complete')
 print('creating train and validation data by dividing train_data in 80:20 ratio')
 permutation = np.random.permutation(len(train_data))
 index_train = permutation[:int(len(train_data)*0.8)]
-index_validation = permutation[int(len(train_
+index_validation = permutation[int(len(train_data)*0.2):]
+
+final_trai
