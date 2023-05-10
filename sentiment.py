a5e70a3cdd05dd6d46f9be6c3a5eac6d7958b9e9
310zzj <310zzj@proton.me>
2023-05-10 16:41:09 +0000

    * small fix

--- sentiment.py
+++ sentiment.py
@@ -266,4 +266,4 @@ embedding_layer = Embedding(nb_words, EMBEDDING_DIM, weights = [embedding_matrix
 lstm_layer = LSTM(num_lstm, dropout = lstm_dropout_rate, recurrent_dropout = lstm_dropout_rate, return_sequences = True )       
 
 input_comment = Input(shape = (MAX_SEQUENCE_LENGTH,), dtype = 'int32')
-embedded
+embedded_sequence = embedding
