c3f6e9a7db2ad8487915f10a58ae2f199c77db1d
310zzj <310zzj@proton.me>
2023-05-11 05:49:11 +0000

    Generated from configure.in

--- sentiment.py
+++ sentiment.py
@@ -267,4 +267,4 @@ lstm_layer = LSTM(num_lstm, dropout = lstm_dropout_rate, recurrent_dropout = lst
 
 input_comment = Input(shape = (MAX_SEQUENCE_LENGTH,), dtype = 'int32')
 embedded_sequence = embedding_layer(input_comment)
-x = lstm_la
+x = lstm_layer(embedded_sequenc
