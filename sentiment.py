0403e447458742758802b117ffa1424098bbda16
310zzj <310zzj@proton.me>
2023-05-15 21:03:18 +0000

    Updated title

--- sentiment.py
+++ sentiment.py
@@ -273,4 +273,4 @@ merged = Attention(MAX_SEQUENCE_LENGTH)(x)
 merged = Dense(num_dense, activation = act)(merged)
 merged = Dropout(dense_dropout_rate)(merged)
 merged = BatchNormalization()(merged)
-preds 
+preds = Dense(len(classes_to_predict), 
