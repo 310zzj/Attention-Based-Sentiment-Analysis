4eb009d0979f0696dafb71135dd1fde34d60554c
310zzj <310zzj@proton.me>
2023-06-02 15:40:02 +0000

    Initialization

--- sentiment.py
+++ sentiment.py
@@ -293,4 +293,5 @@ model_checkpoint = ModelCheckpoint(best_model_path, save_best_only = True, save_
 hist = model.fit(x = final_train_data, y = labels_of_train_data,\
                  validation_data = (final_validation_data, labels_of_validation_data), \
                  epochs = 20, batch_size = 256, shuffle = True, \
-                 callbacks = [early_stopping, model_checkpoin
+                 callbacks = [early_stopping, model_checkpoint])
+best_score = min(hist.h
