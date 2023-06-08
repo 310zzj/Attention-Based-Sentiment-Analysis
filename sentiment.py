a026e61b605b9b57cacf28f1e393f1bb14318e9e
310zzj <310zzj@proton.me>
2023-06-08 21:31:59 +0000

    remove some debugging code

--- sentiment.py
+++ sentiment.py
@@ -301,4 +301,5 @@ best_score = min(hist.history['val_loss'])
 ## time to make prediction!!!
 ########################################
 y_test_predicted = model.predict([final_test_data], batch_size = 1024, verbose = 1)
-sample_submission = pd.read_csv("../input/sample_submissi
+sample_submission = pd.read_csv("../input/sample_submission.csv")
+sample_submission[class
